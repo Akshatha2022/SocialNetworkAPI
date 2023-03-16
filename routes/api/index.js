04d35d9eb8c3a253e69495646f1cba760d02cff1
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-03-16 17:20:21 -0500

    refactoring api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
+// Setting up Express Router requirements 
 const router = require('express').Router();
+
+// Setting up users and thoughts routes 
 const usersRoutes = require('./user-routes');
 const thoughtsRoutes = require('./thought-routes');
 
 // it will add prefix of `/users` to created routes 
 router.use('/users', usersRoutes);
 
-// add prefix of `/thoughts` to created routes 
+// it will add prefix of `/thoughts` to created routes 
 router.use('/thoughts', thoughtsRoutes);
 
+// Export Module Router
 module.exports = router;
