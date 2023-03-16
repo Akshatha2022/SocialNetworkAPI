6262366c0c508ee7b480a4b97c66cc4133fbd85c
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-03-15 20:35:18 -0500

    adding to get specific id and create a new thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -14,4 +14,41 @@ const thoughtsController = {
             res.status(500).json(err);
         });
     },
-}
+},
+
+
+    // Get a specific thought by ID
+    getThoughtsById({ params }, res) {
+        Thoughts.findOne({ _id: params.id })
+        .populate({path: 'reactions',select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtsData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+
+    // Create a new thought below
+    createThoughts({ params, body}, res) {
+        Thoughts.create(body)
+        .then(({ _id }) => {
+            return Users.findOneAndUpdate({ _id: params.userId }, { $push: { thoughts: _id }}, { new: true, runValidators: true });
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({ message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+        
+    }
+
