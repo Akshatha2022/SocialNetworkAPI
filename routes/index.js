363c3503abe9773861d0728f7d03f97ee25c71d4
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-03-15 21:26:09 -0500

    adding description

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+// Importing all of the API routes from /api/index.js 
 const apiRoutes = require('./api');
 
+// adding the prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
