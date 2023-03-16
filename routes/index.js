eb9c88e51c1c640d1897a8d55579d171a864f614
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-03-16 17:32:57 -0500

    refactoring routes index

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+// Require express router
 const router = require('express').Router();
 
 // Importing all of the API routes from /api/index.js 
@@ -6,8 +7,10 @@ const apiRoutes = require('./api');
 // adding the prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
 
+// 404 Status error message
 router.use((req, res) => {
     res.status(404).send('<h1>😝 404 Error!</h1>');
   });
 
+  //module exports router
 module.exports = router;
